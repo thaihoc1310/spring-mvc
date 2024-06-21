4ad2ef4333aaffa4acb18e49a81abb80c00733c6
thaihoc <thaihoc131005@gmail.com>
2024-06-21 20:47:23 +0700

    delete some import have no effect

--- src/main/java/vn/thaihoc/laptopshop/controller/UserController.java
+++ src/main/java/vn/thaihoc/laptopshop/controller/UserController.java
@@ -11,10 +11,7 @@ import vn.thaihoc.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
